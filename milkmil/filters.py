74bebc7ed92546c0dcdba21b3e51ea7908d78671
Pratish7 <pratishbajpai7@gmail.com>
2023-08-16 12:48:36 +0530

    added filter in guest queue api

--- milkmil/filters.py
+++ milkmil/filters.py
@@ -64,6 +64,12 @@ class GuestsInFilter(filters.BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
 
         queryset = queryset.filter(out_date__isnull=True, out_time__isnull=True)
+        if 'type' in request.query_params:
+            if request.query_params['type'] in ['organization', 'relationship']:
+                if request.query_params['type'] == 'organization':
+                    queryset = queryset.filter(relationship__isnull=True)
+                elif request.query_params['type'] == 'relationship':
+                    queryset = queryset.filter(organization__isnull=True)
         return queryset
 
 
